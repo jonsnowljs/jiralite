29ce549b605b37716ca052aebd761f5b781a3089
Jiasheng Lu <1473875731@qq.com>
2022-04-09 23:17:27 -0600

    feat: add authprovider

---
+++ src/AuthProvider.ts
@@ -0,0 +1,44 @@
+import { User } from "screens/ProjectList/SearchPanel";
+
+const localStorageKey = "__auth_provider_token__";
+const apiurl = process.env.REACT_APP_API_URL;
+
+export const getToken = () => window.localStorage.getItem(localStorageKey);
+
+export const handleUserResponse = ({ user }: { user: User }) => {
+  window.localStorage.setItem(localStorageKey, user.token || "");
+  return user;
+};
+
+export const login = (data: { username: string; password: string }) => {
+  return fetch(`${apiurl}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).then(async (response: Response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    } else {
+      return Promise.reject(data);
+    }
+  });
+};
+
+export const register = (data: { username: string; password: string }) => {
+  return fetch(`${apiurl}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then(async (response) => {
+    if (response.ok) {
+      return handleUserResponse(await response.json());
+    } else {
+      return Promise.reject(data);
+    }
+  });
+};
+
+export const logout = async () =>
+  window.localStorage.removeItem(localStorageKey);
